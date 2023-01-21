93f116978a2f377d801fac222f9c2aab46666124
annafrymark <anna.frymark@gmail.com>
2023-01-21 12:45:29 +0100

    task 1 next update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,7 @@
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 const body = document.querySelector('body');
-
+let randomHex;
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
