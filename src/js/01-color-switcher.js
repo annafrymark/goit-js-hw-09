c46104b31d9c211e72b7688e5f9a05ac92e44b25
annafrymark <anna.frymark@gmail.com>
2023-01-21 12:22:33 +0100

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', event => {
+    event.preventDefault();
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    randomHex = setInterval(() => {
+        body.style.backgroundColor = `${getRandomHexColor()}`
+    }, 1000)
+});
+
+stopBtn.addEventListener('click', event => {
+    event.preventDefault();
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+    if (randomHex) {
+        clearInterval(randomHex);
+    }
+});
